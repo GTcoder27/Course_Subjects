38af485a28d6a990b67395f2b5264606635f3797
GTcoder27 <girishnandanwar2708@gmail.com>
2025-12-04 20:05:07 +0530

    done

--- Cilent-Server(RMI)/Arithmetic.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Arithmetic extends Remote {
-    String performOperation(String operation, double num1, double num2) throws RemoteException;
-}
